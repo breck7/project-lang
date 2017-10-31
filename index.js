68dbe7a0eee6e806ba23b29ddb0ec8b05c4220fa
Breck Yunits <breck7@gmail.com>
2017-10-30 18:16:15 -0700

    Updated to Latest Tree

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-module.exports = require("./src/ProjectProgram.js")
+const fs = require("fs")
+const TreeProgram = require("treeprogram")
+
+class ProjectProgram extends TreeProgram {
+  getGrammarString() {
+    return fs.readFileSync(this.getGrammarFilePath(), "utf8")
+  }
+
+  getGrammarFilePath() {
+    return __dirname + "/project.grammar"
+  }
+
+  getOrderedDependenciesArray() {
+    const cloned = this.clone()
+    const sorted = []
+    const included = {}
+    let lastLength
+    while (cloned.length) {
+      if (lastLength === cloned.length) {
+        cloned.getChildren().forEach(file => {
+          console.log(`'${file.getLine()}' is missing '${file.getMissingDependencies(included).join(",")}'`)
+        })
+        throw new Error(
+          `Circular dependency or other error detected with ${cloned.length} remaining. ${cloned.toString()}`
+        )
+      }
+      lastLength = cloned.length
+      for (let index = 0; index < cloned.length; index++) {
+        const file = cloned.nodeAt(index)
+        const missingDependencies = file.getMissingDependencies(included)
+        if (missingDependencies.length === 0) {
+          const path = file.getFilePath()
+          cloned.nodeAt(index).destroy()
+          sorted.push(path)
+          included[path] = true
+          break
+        }
+      }
+    }
+    return sorted
+  }
+
+  static _getRequiredFiles(str) {
+    const regex = /(\n|^)const .* \= require\("([^"]+)"\)/g
+    const regex2 = /"(.+)"/
+    const matches = str.match(regex)
+    if (!matches) return []
+    return matches.map(match => match.match(regex2)[1]).map(file => {
+      let type = "external"
+      if (file.startsWith(".")) type = "relative"
+      else if (file.startsWith("/")) type = "absolute"
+      return `${type} ${file}`
+    })
+  }
+
+  static getProjectProgram(arrayOfScriptPaths) {
+    const files = new TreeProgram(arrayOfScriptPaths.join("\n"))
+    const required = new TreeProgram()
+    files.getChildren().forEach(child => {
+      const line = child.getLine()
+      const content = fs.readFileSync(line, "utf8")
+      const requiredFiles = this._getRequiredFiles(content)
+      required.append(`file ${line}`, requiredFiles.length ? requiredFiles.join("\n") : undefined)
+    })
+    return new ProjectProgram(required.toString())
+  }
+}
+
+module.exports = ProjectProgram
